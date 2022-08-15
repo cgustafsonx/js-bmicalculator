784b91680ed5a36a9d56cdd15e03feb668953f20
cgustafsonx <cgustafsonx@gmail.com>
2022-08-15 09:29:36 +0200

    Finished

---
+++ index.js
@@ -0,0 +1,28 @@
+const readlineSync = require("readline-sync");
+
+const weightInKg = readlineSync.questionInt("What is your weight? ");
+console.log("Your weight is ", weightInKg);
+
+const heightInM = readlineSync.questionFloat("What is your height? ");
+console.log("Your height is ", heightInM);
+
+let bmi = weightInKg / (heightInM * heightInM);
+console.log("Your BMI is", bmi.toFixed(2));
+
+console.log("Smaller than 16?", bmi > 16);
+
+let category;
+
+if (bmi < 16) {
+  category = "Severly Underweight";
+} else if (bmi >= 16 && bmi <= 18.4) {
+  category = "Underweight";
+} else if (bmi >= 18.5 && bmi <= 24.9) {
+  category = "Normal";
+} else if (bmi >= 25 && bmi <= 29.9) {
+  category = "Overweight";
+} else {
+  category = "Obese";
+}
+
+console.log(category);
